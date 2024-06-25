c2f40ce48d865d8ed9b71d48d1bc2a610900f37c
Kartik <159441590+Kartikpatel0910@users.noreply.github.com>
2024-06-25 11:01:22 +0530

    Delete kk directory

--- kk/common_widgets_demo/android/app/src/main/kotlin/com/example/common_widgets_demo/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.common_widgets_demo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- kk/snackbar/snackbar_demo/android/app/src/main/kotlin/com/example/snackbar_demo/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.snackbar_demo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()

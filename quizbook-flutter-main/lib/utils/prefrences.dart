6c355c0bb5d5beb1c55a477648c920d30861047b
Kartik <159441590+Kartikpatel0910@users.noreply.github.com>
2024-06-25 10:56:00 +0530

    Delete quizbook-flutter-main directory

--- quizbook-flutter-main/lib/utils/prefrences.dart
+++
@@ -1,7 +0,0 @@
-class Preferences {
-  static const userLogin = "userLogin";
-  static const token = "token";
-  static const userID = "userID";
-  static const id = "id";
-  static const userFullDetails = "userFullDetails";
-}
